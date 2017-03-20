46aaee6fcc27cf1900bca3435fd522bbde2476e4
Edward Faulkner <ef@alum.mit.edu>
2017-03-19 23:57:22 -0400

    fix test

--- tests/integration/components/overlay-marks-test.js
+++ tests/integration/components/overlay-marks-test.js
@@ -5,12 +5,25 @@ moduleForComponent('overlay-marks', 'Integration | Component | overlay marks', {
   integration: true
 });
 
-test('it yields marks with matching type', function(assert) {
+test('it yields marks with default group', function(assert) {
   this.render(hbs`
-    {{#mark-overlay type="test" id="my-mark-id"}}
+    {{#mark-overlay id="my-mark-id"}}
       <div class="test-target"></div>
     {{/mark-overlay}}
-    {{#overlay-marks type="test" as |mark|}}
+    {{#overlay-marks as |mark|}}
+      <div class="test-overlay">{{mark.id}}</div>
+    {{/overlay-marks}}
+  `);
+  assert.equal(this.$('.test-overlay').text(), 'my-mark-id');
+});
+
+
+test('it yields marks with matching group', function(assert) {
+  this.render(hbs`
+    {{#mark-overlay group="test" id="my-mark-id"}}
+      <div class="test-target"></div>
+    {{/mark-overlay}}
+    {{#overlay-marks group="test" as |mark|}}
       <div class="test-overlay">{{mark.id}}</div>
     {{/overlay-marks}}
   `);
@@ -19,10 +32,10 @@ test('it yields marks with matching type', function(assert) {
 
 test('it ignores marks with other types', function(assert) {
   this.render(hbs`
-    {{#mark-overlay type="other" id="my-mark-id"}}
+    {{#mark-overlay group="other" id="my-mark-id"}}
       <div class="test-target"></div>
     {{/mark-overlay}}
-    {{#overlay-marks type="test" as |mark|}}
+    {{#overlay-marks group="test" as |mark|}}
       <div class="test-overlay">{{mark.id}}</div>
     {{/overlay-marks}}
   `);
