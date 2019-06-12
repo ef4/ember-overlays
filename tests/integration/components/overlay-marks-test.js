d982c30f52b4ff1386558efaae86bf7fc8003fa4
Edward Faulkner <ef@alum.mit.edu>
2019-06-12 15:21:04 -0400

    codemods

--- tests/integration/components/overlay-marks-test.js
+++ tests/integration/components/overlay-marks-test.js
@@ -1,43 +1,45 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('overlay-marks', 'Integration | Component | overlay marks', {
-  integration: true
-});
+module('Integration | Component | overlay marks', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it yields marks with default group', function(assert) {
-  this.render(hbs`
-    {{#mark-overlay id="my-mark-id"}}
-      <div class="test-target"></div>
-    {{/mark-overlay}}
-    {{#overlay-marks as |mark|}}
-      <div class="test-overlay">{{mark.id}}</div>
-    {{/overlay-marks}}
-  `);
-  assert.equal(this.$('.test-overlay').text(), 'my-mark-id');
-});
+  test('it yields marks with default group', async function(assert) {
+    await render(hbs`
+      {{#mark-overlay id="my-mark-id"}}
+        <div class="test-target"></div>
+      {{/mark-overlay}}
+      {{#overlay-marks as |mark|}}
+        <div class="test-overlay">{{mark.id}}</div>
+      {{/overlay-marks}}
+    `);
+    assert.dom('.test-overlay').hasText('my-mark-id');
+  });
 
 
-test('it yields marks with matching group', function(assert) {
-  this.render(hbs`
-    {{#mark-overlay group="test" id="my-mark-id"}}
-      <div class="test-target"></div>
-    {{/mark-overlay}}
-    {{#overlay-marks group="test" as |mark|}}
-      <div class="test-overlay">{{mark.id}}</div>
-    {{/overlay-marks}}
-  `);
-  assert.equal(this.$('.test-overlay').text(), 'my-mark-id');
-});
+  test('it yields marks with matching group', async function(assert) {
+    await render(hbs`
+      {{#mark-overlay group="test" id="my-mark-id"}}
+        <div class="test-target"></div>
+      {{/mark-overlay}}
+      {{#overlay-marks group="test" as |mark|}}
+        <div class="test-overlay">{{mark.id}}</div>
+      {{/overlay-marks}}
+    `);
+    assert.dom('.test-overlay').hasText('my-mark-id');
+  });
 
-test('it ignores marks with other types', function(assert) {
-  this.render(hbs`
-    {{#mark-overlay group="other" id="my-mark-id"}}
-      <div class="test-target"></div>
-    {{/mark-overlay}}
-    {{#overlay-marks group="test" as |mark|}}
-      <div class="test-overlay">{{mark.id}}</div>
-    {{/overlay-marks}}
-  `);
-  assert.equal(this.$('.test-overlay').length, 0, 'should find none');
+  test('it ignores marks with other types', async function(assert) {
+    await render(hbs`
+      {{#mark-overlay group="other" id="my-mark-id"}}
+        <div class="test-target"></div>
+      {{/mark-overlay}}
+      {{#overlay-marks group="test" as |mark|}}
+        <div class="test-overlay">{{mark.id}}</div>
+      {{/overlay-marks}}
+    `);
+    assert.dom('.test-overlay').doesNotExist('should find none');
+  });
 });
