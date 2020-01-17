cbab8c25402e8cfe5f545263d9a1eb256b65610a
0xADADA <0xadada.pub@0xadada.pub>
2020-01-17 15:54:23 -0500

    Made reverseOrder the default, removed the attribute

--- addon/components/overlay-marks.js
+++ addon/components/overlay-marks.js
@@ -10,7 +10,10 @@ export default Component.extend({
   marks: computed('service.marks', 'reverseOrder', function() {
     let group = this.get('group') || 'default';
     let id = this.get('id');
-    const marks = this.get('service.marks').filter(m => m.group === group && (id == null || m.id === id))
-    return this.get('reverseOrder') ? marks.slice(0).reverse() : marks;
+    const marks = this.get('service.marks')
+      .filter(m => m.group === group && (id == null || m.id === id))
+      .slice(0)
+      .reverse();
+    return marks;
   })
 });
--- tests/integration/components/overlay-marks-test.js
+++ tests/integration/components/overlay-marks-test.js
@@ -42,7 +42,7 @@ module('Integration | Component | overlay marks', function(hooks) {
     assert.dom('.test-overlay').doesNotExist('should find none');
   });
 
-  test('it renders the marks in the order they are registered', async function(assert) {
+  test('it renders the marks in reverse order to correct z-index', async function(assert) {
     await render(hbs`
       {{#mark-overlay group="other" id="my-mark-id-1"}}
         <div class="test-target"></div>
@@ -51,23 +51,6 @@ module('Integration | Component | overlay marks', function(hooks) {
         <div class="test-target"></div>
       {{/mark-overlay}}
       {{#overlay-marks group="other" as |mark|}}
-        <div class="test-overlay">{{mark.id}}</div>
-      {{/overlay-marks}}
-    `);
-    assert.dom('.test-overlay').exists({ count: 2 });
-    assert.dom('.test-overlay').hasText('my-mark-id-1');
-    assert.dom('.test-overlay~.test-overlay').hasText('my-mark-id-2');
-  });
-
-  test('it reverses the order of the marks', async function(assert) {
-    await render(hbs`
-      {{#mark-overlay group="other" id="my-mark-id-1"}}
-        <div class="test-target"></div>
-      {{/mark-overlay}}
-      {{#mark-overlay group="other" id="my-mark-id-2"}}
-        <div class="test-target"></div>
-      {{/mark-overlay}}
-      {{#overlay-marks group="other" reverseOrder=true as |mark|}}
         <div class="test-overlay">{{mark.id}}</div>
       {{/overlay-marks}}
     `);
